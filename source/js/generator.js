f3535fb6579bef365e655bc324945baf36eed8d9
ccmoralesj <ccmj202@gmail.com>
2016-05-06 19:54:11 -0500

    Adding materialize

--- source/js/generator.js
+++ source/js/generator.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById("field");
+const canvas_width = canvas.width;
+const canvas_height = canvas.height;
 const context = canvas.getContext("2d");
 const pixel = 10;
 var rectangles = [];
@@ -22,31 +24,32 @@ function saveRect(x,y){
 
 function erasePixel(x,y){
     "use strict";
-    rectangles = rectangles.filter(function(rect){ return !(rect["x"] === x && rect["y"] === y); });
+    rectangles = rectangles.filter(function(rect){ return !(rect["x"] === x && rect["y"] === y);});
 }
 
 function isRect(x,y){
-	if( x < 0 || x > canvas.width || y < 0 || y > canvas.height) return true;
+	if( x < 0 || x > canvas_width || y < 0 || y > canvas_height) return true;
 	var rect = rectangles.filter(function(rect){ return (rect["x"] === x && rect["y"] === y); });
 
     return (rect.length)? true : false;
 }
 
-function mayorityRule(x,y){
+function majorityRule(x,y){
 		return isRect(x - pixel,y - pixel) + isRect(x,y - pixel) + isRect(x + pixel,y - pixel) + isRect(x - pixel,y) + isRect(x,y) 
 				+ isRect(x + pixel,y) + isRect(x - pixel,y + pixel) + isRect(x,y + pixel) + isRect(x + pixel,y + pixel);
 }
 
 function emptyGrid(){
 	rectangles = [];
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.clearRect(0, 0, canvas_width, canvas_height);
+    setPercentage(0);
 }
 
 function randomBase(){
+	emptyGrid();
 	let RN;
-	rectangles = [];
-	for(let i = 0; i <= canvas.height ; i+= pixel){
-		for(let j = 0; j <= canvas.width ; j += pixel){
+	for(let i = 0; i <= canvas_height ; i+= pixel){
+		for(let j = 0; j <= canvas_width ; j += pixel){
 			RN = Math.random();
 			//console.log("here");
 			if(RN >= 0.5){
@@ -58,9 +61,9 @@ function randomBase(){
 }
 
 function step(){
-	for(let i = 0; i <= canvas.height ; i+= pixel){
-		for(let j = 0; j <= canvas.width ; j += pixel){
-			let mayority = mayorityRule(j,i);
+	for(let i = 0; i <= canvas_height ; i+= pixel){
+		for(let j = 0; j <= canvas_width ; j += pixel){
+			let mayority = majorityRule(j,i);
 			if(mayority <= 4){
 				erasePixel(j,i);
 			}else{
@@ -72,28 +75,48 @@ function step(){
 }
 
 function run(){
-	let diff,rectsInGrid = 0;
+	let percentage, diff,rectsInGrid = 0;
     diff = 0;
 	while(rectsInGrid !== rectangles.length){
 		rectsInGrid = rectangles.length;
 		step();
-        diff = Math.abs(rectsInGrid - rectangles.length);
-        console.log(rectsInGrid,"!=",rectangles.length, diff, (100 - (diff/rectangles.length) * 100) +"%");
 
+		// Percentage of the dungeon generator process
+		diff = Math.abs(rectsInGrid - rectangles.length);
+		percentage = (100 - (diff/rectangles.length) * 100);
+		setPercentage(percentage);
+		//----------------------------------------------
     }
-    console.log(rectsInGrid,"!=",rectangles.length, diff);
 
     drawDungeon();
 }
 
 function drawDungeon(){
     "use strict";
-    for(let i = 0; i <= canvas.height ; i+= pixel){
-        for(let j = 0; j <= canvas.width ; j += pixel){
+    for(let i = 0; i <= canvas_height ; i+= pixel){
+        for(let j = 0; j <= canvas_width ; j += pixel){
             if(isRect(j,i))
                 alivePixel(j,i);
             else
                 deathPixel(j,i);
         }
     }
+}
+
+function setPercentage(percentage){
+	"use strict";
+	let percentageBar = document.getElementById("percentage_bar");
+	percentageBar.style.width = percentage + "%";
+}
+
+function fillingCanvas() {
+    "use strict";
+    //alivePixel(50,50);
+    document.getElementsByClassName("row")[1].innerHTML += "<div>Hola</div>";
+    requestAnimationFrame(fillingCanvas);
+}
+
+function fillG(){
+    "use strict";
+    requestAnimationFrame(fillingCanvas);
 }
